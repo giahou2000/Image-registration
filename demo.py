121adea385ed79e5c6bd057ec215578303654fb1
Christos <giachoud@gapps.auth.gr>
2023-07-05 15:42:50 +0300

    Descriptors demo ready

--- demo.py
+++ demo.py
@@ -10,10 +10,47 @@ rhom = 5
 rhoM = 20
 rhostep = 1
 N = 8
+
+# Descriptors for the (100, 100) point
 p = [100, 100]
 
+print("")
+print("______(100, 100)______")
+print("")
+print("myLocalDescriptor:")
 d = myLocalDescriptor(image, p, rhom, rhoM, rhostep, N)
 print(d)
+print("")
+print("myLocalDescriptorUpgrade:")
+d = myLocalDescriptorUpgrade(image, p, rhom, rhoM, rhostep, N)
+print(d)
 
+
+# Descriptors for the (200, 200) point
+p = [200, 200]
+
+print("")
+print("______(200, 200)______")
+print("")
+print("myLocalDescriptor:")
+d = myLocalDescriptor(image, p, rhom, rhoM, rhostep, N)
+print(d)
+print("")
+print("myLocalDescriptorUpgrade:")
+d = myLocalDescriptorUpgrade(image, p, rhom, rhoM, rhostep, N)
+print(d)
+
+
+# Descriptors for the (202, 202) point
+p = [202, 202]
+
+print("")
+print("______(202, 202)______")
+print("")
+print("myLocalDescriptor:")
+d = myLocalDescriptor(image, p, rhom, rhoM, rhostep, N)
+print(d)
+print("")
+print("myLocalDescriptorUpgrade:")
 d = myLocalDescriptorUpgrade(image, p, rhom, rhoM, rhostep, N)
 print(d)
